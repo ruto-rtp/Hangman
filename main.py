07a9536b142801db25bb3b25c043adb221183704
NIU1742588 <p.ruiz.t.24@gmail.com>
2026-02-23 00:20:08 +0100

    Add game logic and dictionary for Hangman implementation
    Signed-off-by: NIU1742588 <p.ruiz.t.24@gmail.com>

---
+++ main.py
@@ -0,0 +1,97 @@
+from random import randint, random
+import sys
+from time import sleep
+import os
+
+
+
+def clean_console():
+    """Cleans the console depending on your operative system"""
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def hanged(fails):
+    # "  +---+",
+    # "  |   |",
+    # "  O   |",
+    # " /|\  |",
+    # " / \  |",
+    # "      |",
+    # "======="
+    hanged = ["  +---+",
+              "  |   |",
+              "      |",
+              "      |",
+              "      |",
+              "      |",
+              "======="]
+    
+    if fails >= 1:
+        hanged[2] = "  O   |"
+    if fails >= 2:
+        hanged[3] = "  |   |"
+    if fails >= 3:
+        hanged[3] = " /|   |"
+    if fails >= 4:
+        hanged[3] = " /|\  |"
+    if fails >= 5:
+        hanged[4] = " /    |"
+    if fails >= 6:
+        hanged[4] = " / \  |"
+
+    return hanged
+
+def main():
+    clean_console()
+    d = open("./[Python] hanged/diccionario.txt", "r")
+    words = d.read()
+    d.close()
+
+    # ----- Seleccion de la palabra aleatoria -----
+    random_index = randint(0, len(words.split("\n")) - 1)
+    word = words.split("\n")[random_index] # Seleccionamos la palabra aleatoria
+    len_word = len(word) 
+    unknown_word = ["_"] * len_word # Crear la palabra con "_"
+    fails = 0
+
+    # Mientras no se adivine la palabra
+    while unknown_word != list(word) and fails < 6:
+        print("--- AHORCADO ---")
+
+        # ----- Dibujo del ahorcado -----
+        hanged_print = hanged(fails)
+        for line in hanged_print:
+            print(line)
+
+        # ----- Muestra la palabra a adivinar -----
+        print(word)
+        for i in range(0, len_word):
+            print(unknown_word[i], end=" ")
+        print()
+
+        # ----- Comprobación de la letra -----
+        letter = input("Introduce una letra: ").strip().lower()
+
+        if letter in word:
+            for i in range(0, len_word):
+                if word[i] == letter:
+                    unknown_word[i] = letter
+        else:
+            fails += 1
+            print(f"La letra {letter} no está en la palabra")
+        
+        sleep(1)
+        clean_console()
+
+    # ----- Menasaje final -----
+    print("--- AHORCADO ---")
+    if fails == 6:
+        print("Has perdido! La plabra era:", word)
+    else:
+        print("Felicidades! Has ganado! La palabra era:", word)
+
+
+if __name__ == "__main__":
+    
+
+    # print(LANGUAGES)
+    main()
